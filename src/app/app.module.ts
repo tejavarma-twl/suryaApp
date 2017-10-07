cd0d38f870f3da861a5fd3c61e442607eb9148a2
Teja Varma <tejavarma@Tejas-MacBook-Pro.local>
2017-10-07 18:18:22 +0530

    added news in sidenote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { SidenoteComponent } from './sidenote/sidenote.component';
+import { NewsComponent } from './sidenote/news/news.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    SidenoteComponent
+    SidenoteComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule
